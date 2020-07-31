5491e04cc4863ecc46f49fbb981ca88d16aeac32
Neil Leonard <najleonard@gmail.com>
2020-07-31 16:20:24 -0700

    ran yarn build 8

--- src/examples/sankey/sankey.ts
+++ src/examples/sankey/sankey.ts
@@ -70,7 +70,7 @@ const vis: Sankey = {
     const svg = this.svg
       .html('')
       .attr('width', '100%')
-      .attr('height', '95%')
+      .attr('height', '100%')
       .append('g')
 
     const dimensions = queryResponse.fields.dimension_like
